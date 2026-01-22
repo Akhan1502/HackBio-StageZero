30b6d6fa621a8ddccd78382fd9c75c7f80d118ca
Aqsa Khan <thisisonlyworkrelated123@gmail.com>
2026-01-22 14:18:15 +0400

    Uploaded R script file

---
+++ simple_code.R
@@ -0,0 +1,14 @@
+# Define personal information
+name <- "Aqsa Khan"
+affiliation <- "Amity University Dubai"
+favorite_gene <- "cry1Ab"
+organism <- "Bacillus thuringiensis"
+
+# Print the formatted sentence
+cat(
+  "Hi, my name is", name,
+  ", a student and researcher at the", affiliation,
+  ". My favorite gene is", favorite_gene,
+  "found in", organism, ".\n"
+)
+
